a22ccc582b8654f7171d9e800b6dfe4412fec71d
Anton Lyamin <z5160328@student.unsw.edu.au>
2018-07-31 21:36:22 +1000

    Made progress on fib.py

--- fib.py
+++ fib.py
@@ -6,22 +6,29 @@ TOPICS = lists, variables, loops
 
 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
 '''
+n = int(input("enter a number: "))
 
-def produceFibsList(n):
-    '''
-    >>> produceFibsList(0)
-    []
-    >>> produceFibsList(1)
-    [1]
-    >>> produceFibsList(2)
-    [1, 1]
-    >>> produceFibsList(3)
-    [1, 1, 2]
-    >>> produceFibsList(5)
-    [1, 1, 2, 3, 5]
-    '''
-    # TODO = fill in the code here, and return the correct result using the return keyword
-    pass
+  
+fiblist = []
+i = 1
+
+if n == 0:
+	print(fiblist)
+else: 
+	while i < (n + 1):
+		if i == 1:
+			fiblist.append(i)
+		elif i == 2:
+			fiblist.append(1)
+		else:
+			a = fiblist.pop()
+			b = fiblist.pop()
+			fiblist.append(a)
+			fiblist.append(b)
+			fiblist.append(a+b)
+		i += 1
+	
+	print(fiblist)
 
 if __name__ == '__main__':
     import doctest
